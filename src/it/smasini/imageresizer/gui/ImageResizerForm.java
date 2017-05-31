4f283e6ec89289bc31d085ead5b733bd9ab71c66
smasini <simone.masini92@gmail.com>
2017-05-31 17:25:52 +0200

    Form
    add features for the form

--- src/it/smasini/imageresizer/gui/ImageResizerForm.java
+++ src/it/smasini/imageresizer/gui/ImageResizerForm.java
@@ -1,13 +1,23 @@
 package it.smasini.imageresizer.gui;
 
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.impl.file.PsiDirectoryFactory;
 import it.smasini.imageresizer.files.Renamer;
 import it.smasini.imageresizer.images.Resizer;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 /**
  * Created by Simone on 29/05/17.
@@ -17,6 +27,26 @@ public class ImageResizerForm {
     private JRadioButton radioSingleImage;
     private JPanel panel;
     private JButton buttonStartResize;
+    private JTextField textFieldInputImages;
+    private JButton buttonSelectImage;
+    private JTextField textFieldResFolder;
+    private JButton buttonSelectResFolder;
+    private JLabel labelOutput;
+    private JCheckBox checkBoxRename;
+    private JPanel panelRename;
+    private JCheckBox replaceCheckBox;
+    private JTextField textFieldPattern;
+    private JTextField textFieldReplaceWith;
+    private JCheckBox addAtStartCheckBox;
+    private JTextField textFieldAddStart;
+    private JCheckBox addAtEndCheckBox;
+    private JTextField textFieldAddEnd;
+    private JPanel panelSettingSingleImage;
+    private JComboBox comboBoxTypeScale;
+    private JTextField textFieldPxW;
+    private JTextField textFieldPxH;
+    private JLabel labelHeigth;
+    private JLabel labelWidth;
 
     private JFrame frame;
     private Project project;
@@ -27,6 +57,7 @@ public class ImageResizerForm {
         ButtonGroup group = new ButtonGroup();
         group.add(radioRes);
         group.add(radioSingleImage);
+
     }
 
     public void show() {
@@ -44,6 +75,13 @@ public class ImageResizerForm {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Renamer renamer = new Renamer();
+                renamer.setAddAtStart(addAtStartCheckBox.isSelected());
+                renamer.setAddAtEnd(addAtEndCheckBox.isSelected());
+                renamer.setReplace(replaceCheckBox.isSelected());
+                renamer.setStartString(textFieldAddStart.getText());
+                renamer.setEndString(textFieldAddEnd.getText());
+                renamer.setReplacePattern(textFieldPattern.getText());
+                renamer.setReplaceWith(textFieldReplaceWith.getText());
 
                 Resizer resizer = new Resizer("/Users/Simone/Desktop/workspace/ic_time_lock.png", radioRes.isSelected(), renamer);
                 resizer.scale();
@@ -52,5 +90,109 @@ public class ImageResizerForm {
                 frame.dispose();
             }
         });
+        ChangeListener radioChange = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                setOutputText();
+            }
+        };
+        radioRes.addChangeListener(radioChange);
+        radioSingleImage.addChangeListener(radioChange);
+        checkBoxRename.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if(checkBoxRename.isSelected()){
+                    panelRename.setVisible(true);
+                }else{
+                    panelRename.setVisible(false);
+                }
+            }
+        });
+        buttonSelectImage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showFileChoicerForImages();
+            }
+        });
+        buttonSelectResFolder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showFileChoicerForDirectory();
+            }
+        });
+        comboBoxTypeScale.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                boolean percentage = comboBoxTypeScale.getSelectedItem().toString().equals("%");
+                if(percentage){
+                    labelWidth.setText("percentage");
+                    textFieldPxH.setVisible(false);
+                    labelHeigth.setVisible(false);
+                }else{
+                    labelWidth.setText("Width");
+                    textFieldPxH.setVisible(true);
+                    labelHeigth.setVisible(true);
+                }
+            }
+        });
     }
+
+    private void setOutputText(){
+        if(radioRes.isSelected()){
+            labelOutput.setText("Res Folder");
+            panelSettingSingleImage.setVisible(false);
+        }else{
+            labelOutput.setText("Output Folder");
+            panelSettingSingleImage.setVisible(true);
+        }
+    }
+
+    private void showFileChoicerForImages() {
+        FileChooserDescriptor descriptor = new FileChooserDescriptor(true, false, false, false, false, true);
+        VirtualFile virtualFile = FileChooser.chooseFile(descriptor, WindowManager.getInstance().findVisibleFrame(), project, null);
+        if (virtualFile != null) {
+            //PsiDirectory directory = PsiDirectoryFactory.getInstance(project).createDirectory(virtualFile);
+
+            /*mainClassName = directory.getName();
+            char[] chars = mainClassName.toCharArray();
+            chars[0] = Character.toUpperCase(chars[0]);
+            rootName.setText(String.valueOf(chars));*/
+
+            //parser.reset(project, directory);
+            String path = virtualFile.getPath();
+            //String pkg = "";
+            textFieldInputImages.setText(path);
+
+            /*if (path.contains("java")) {
+                pkg = path.split("java/")[1].replaceAll("/", ".");
+            } else if (path.contains("src")) {
+                pkg = path.split("src/")[1].replaceAll("/", ".");
+            }
+            basePath = path + "/";
+            pkgText.setText(pkg);
+
+            */
+            //Logger.info("Target path " + path);
+        } else {
+            //Logger.warn("Empty target path!");
+        }
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+    }
+
+    private void showFileChoicerForDirectory() {
+        FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
+        VirtualFile virtualFile = FileChooser.chooseFile(descriptor, WindowManager.getInstance().findVisibleFrame(), project, null);
+        if (virtualFile != null) {
+            //PsiDirectory directory = PsiDirectoryFactory.getInstance(project).createDirectory(virtualFile);
+            String path = virtualFile.getPath();
+            textFieldResFolder.setText(path);
+            //Logger.info("Target path " + path);
+        } else {
+            //Logger.warn("Empty target path!");
+        }
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+    }
+
 }
